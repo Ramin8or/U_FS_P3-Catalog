fe622acb0d8403d79c039229623efc7791643c67
Ramin Halviatti <ramin@outlook.com>
2015-11-03 12:42:23 -0800

    Views using Bootstrap

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -18,7 +18,10 @@ import requests
 
 app = Flask(__name__)
 
-#TODO change these
+# Constants
+ALL_CATEGORIES_ID = 1
+ALL_CATEGORIES = "All Categories"
+SHOW_LIMIT = 12 # Limit for number of recent tems shown in catalog
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Catalog Application"
@@ -37,6 +40,7 @@ def showLogin():
     login_session['state'] = state
     return render_template('login.html', STATE=state)
 
+# Handle connect
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
     # Validate state token
@@ -45,7 +49,6 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     # Obtain authorization code
-    print "Obtain authorization code"
     code = request.data
     try:
         # Upgrade the authorization code into a credentials object
@@ -57,9 +60,7 @@ def gconnect():
             json.dumps('Failed to upgrade the authorization code.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-
     # Check that the access token is valid.
-    print "Check that the access token is valid."
     access_token = credentials.access_token
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
            % access_token)
@@ -69,26 +70,21 @@ def gconnect():
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
-
     # Verify that the access token is used for the intended user.
-    print "Verify that the access token is used for the intended user."
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
         response = make_response(
             json.dumps("Token's user ID doesn't match given user ID."), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-
     # Verify that the access token is valid for this app.
-    print "Verify that the access token is valid for this app."
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
+        print "Token's client ID does not match the app's."
         response.headers['Content-Type'] = 'application/json'
         return response
-
-    print "Is current user already connected?"
+    # Is current user already connected?
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
@@ -96,33 +92,23 @@ def gconnect():
                                  200)
         response.headers['Content-Type'] = 'application/json'
         return response
-
     # Store the access token in the session for later use.
-    print "Store the access token in the session for later use."
     login_session['credentials'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
-
     # Get user info
-    print "Get user info"
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
-
     data = answer.json()
-
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
-    # ADD PROVIDER TO LOGIN SESSION
     login_session['provider'] = 'google'
-
     # see if user exists, if it doesn't make a new one
-    print "see if user exists, if it doesn't make a new one"
     user_id = getUserID(data["email"])
     if not user_id:
         user_id = createUser(login_session)
     login_session['user_id'] = user_id
-
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
@@ -131,7 +117,6 @@ def gconnect():
     output += login_session['picture']
     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
-    print "done!"
     return output
 
 # User Helper Functions
@@ -154,7 +139,7 @@ def getUserID(email):
     except:
         return None
 
-# DISCONNECT - Revoke a current user's token and reset their login_session
+# Handle Disconnect - Revoke a current user's token and reset their login_session
 @app.route('/gdisconnect')
 def gdisconnect():
     # Only disconnect a connected user.
@@ -176,16 +161,6 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-# Show all 
-@app.route('/')
-def showCatalog():
-    categories = db_session.query(Category).order_by(asc(Category.name))
-    latest_items = db_session.query(Item).order_by(desc(Item.id))
-    if 'username' not in login_session:
-        return render_template('catalog.html', categories=categories, latest_items=latest_items)
-    else:
-        return render_template('catalog.html', categories=categories, latest_items=latest_items)
-
 # Disconnect based on provider
 @app.route('/disconnect')
 def disconnect():
@@ -194,9 +169,6 @@ def disconnect():
             gdisconnect()
             del login_session['gplus_id']
             del login_session['credentials']
-        if login_session['provider'] == 'facebook':
-            fbdisconnect()
-            del login_session['facebook_id']
         del login_session['username']
         del login_session['email']
         del login_session['picture']
@@ -208,7 +180,58 @@ def disconnect():
         flash("You were not logged in")
         return redirect(url_for('showCatalog'))
 
+# Show catalog
+@app.route('/')
+@app.route('/catalog')
+def showCatalog():
+    categories = db_session.query(Category).order_by(asc(Category.name))
+    all_cat = categories.filter_by(name=ALL_CATEGORIES).one()
+    items = db_session.query(
+            Item).order_by(desc(Item.id)).limit(SHOW_LIMIT).all()
+    return render_template('catalog.html', 
+                            categories=categories, 
+                            category=all_cat, items=items)
 
+# Show items for selected category
+@app.route('/catalog/<category_name>/')
+def showCategory(category_name):
+    categories = db_session.query(Category).order_by(asc(Category.name))
+    category = categories.filter_by(name=category_name).one()
+    if category.id == ALL_CATEGORIES_ID:
+        items = db_session.query(Item).all()
+    else:
+        items = db_session.query(Item).filter_by(category_id=category.id).all()
+    count = len(items)
+    return render_template('category.html', 
+                            categories=categories, 
+                            category=category, items=items, 
+                            count=count)
+
+# Show item
+@app.route('/catalog/<category_name>/<item_name>')
+def showItem(item_name, category_name):
+    item = db_session.query(Item).filter_by(name=item_name).one()
+    return render_template('item.html', item=item, category_name=category_name)
+
+# Add a new item 
+@app.route('/catalog/newItem/', methods=['GET', 'POST'])
+def newItem():
+    pass
+    return "New"
+
+# Edit an item using POST for safety
+@app.route('/catalog/<item_name>/edit', methods=['GET', 'POST'])
+def editItem(item_name):
+    pass
+    return "Edit"
+
+# Delete an item using POST for safety
+@app.route('/catalog/<item_name>/delete', methods=['GET', 'POST'])
+def deleteItem(item_name):
+    pass
+    return "Delete"
+
+# Main application
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
